9187a34801510854fcbcad34376be8d4ddd3e07d
Adam Shannon <adamkshannon@gmail.com>
2018-09-17 17:54:58 -0500

    envs/sbx: standup GKE cluster and traefik

---
+++ envs/sbx/gcp.tf
@@ -0,0 +1,58 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = "${file(var.gcp_creds_filepath)}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+variable "gcp_creds_filepath" {
+  default = "~/.google/credentials.json"
+  description = "Local filepath for Google Cloud credentials"
+}
+
+variable "gcp_project" {
+  default = "automated-clearing-house"
+  description = "Google Cloud project name"
+}
+
+variable "gcp_region" {
+  default = "us-central1"
+}
+
+variable "gcp_zones" {
+  type = "list"
+  default = [
+    "us-central1-a",
+    "us-central1-b",
+    "us-central1-c",
+  ]
+}
+
+resource "random_shuffle" "zones" {
+  input = ["${var.gcp_zones}"]
+}
+
+// ClusterRole on GKE/K8S 1.6+ (with RBAC) won't let you create roles right away.
+// See the following links:
+//
+// https://github.com/terraform-providers/terraform-provider-kubernetes/pull/1#issuecomment-307940033
+// https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+//
+// https://cloud.google.com/container-engine/docs/role-based-access-control
+// https://github.com/coreos/prometheus-operator/blob/master/Documentation/troubleshooting.md
+//
+// To work around this, let's just shell out and create the CRB's ourself.
+resource "null_resource" "rbac_setup" {
+  count = "${length(var.cluster_admins)}"
+
+  # kubectl create clusterrolebinding myname-cluster-admin-binding --clusterrole=cluster-admin --user=myname@example.org
+  provisioner "local-exec" {
+    command = "kubectl create clusterrolebinding ${var.cluster_name}-admin-binding --clusterrole=cluster-admin --user=${element(var.cluster_admins, count.index)}"
+  }
+}
+variable "cluster_admins" {
+  default = [
+    "adam@moov.io",
+    # "wade@moov.io"
+  ]
+}
--- envs/sbx/kubernetes.tf
+++ envs/sbx/kubernetes.tf
@@ -1,5 +1,19 @@
-variable "kubernetes_username" {}
-variable "kubernetes_password" {}
+variable "cluster_name" {
+  default = "sbx"
+}
+
+variable "username" {}
+variable "password" {}
+
+variable "primary_pool_node_count" {
+  default = 0
+}
+variable "secondary_pool_node_count" {
+  default = 0
+}
+variable "secondary_pool_count" {
+  default = 0
+}
 
 variable "node_disk_size_gb" {
   default = 25
@@ -7,29 +21,36 @@ variable "node_disk_size_gb" {
 variable "node_disk_type" {
   default = "pd-standard"
 }
-variable "node_machine_counts" {
-  type = "map"
-  default = {
-    "g1-small" = 0
-    "n1-standard-1" = 0
-  }
+
+variable "node_machine_type" {
+  default = "g1-small"
 }
+
 variable "node_preemptible" {
-  default = false
+  default = true
 }
+
+variable "min_master_version" {
+  default = "1.10.7-gke.1"
+}
+
+locals {
+  # random_shuffle.zones returns with one of the original zones removed,
+  # which becomes our primary zone.
+  primary_gcp_zone = "${element(random_shuffle.zones.result, 0)}"
+
+  # secondary_gcp_zones is a list of var.gcp_zones, but with the primary removed.
+  secondary_gcp_zones = "${compact(split(",", replace(join(",", random_shuffle.zones.result), "${local.primary_gcp_zone}", "")))}"
+}
+
 
 # Setup for a GCP kubernetes cluster.
-resource "google_container_cluster" "sbx" {
-  name               = "sbx"
-  zone               = "us-central1-b"
-  initial_node_count = 3
+resource "google_container_cluster" "primary" {
+  name               = "${var.cluster_name}"
+  zone               = "${local.primary_gcp_zone}"
+  initial_node_count = 1
 
-  additional_zones = [
-    "us-central1-a",
-    "us-central1-c",
-  ]
-
-  min_master_version = "1.10.7-gke.1" # TODO(adam): This will change overtime.
+  min_master_version = "${var.min_master_version}"
 
   maintenance_policy {
     daily_maintenance_window {
@@ -38,18 +59,14 @@ resource "google_container_cluster" "sbx" {
   }
 
   master_auth {
-    username = "${var.kubernetes_username}"
-    password = "${var.kubernetes_password}"
-  }
-
-  node_pool {
-    name = "sbx-initial-node-pool"
+    username = "${var.username}"
+    password = "${var.password}"
   }
 
   node_config {
     disk_size_gb = "${var.node_disk_size_gb}"
     disk_type    = "${var.node_disk_type}"
-    machine_type = "${lookup(var.node_machine_counts, "g1-small")}"
+    machine_type = "${var.node_machine_type}"
     preemptible  = "${var.node_preemptible}"
 
     oauth_scopes = [
@@ -61,16 +78,16 @@ resource "google_container_cluster" "sbx" {
   }
 }
 
-resource "google_container_node_pool" "sbx" {
-  name       = "sbx-node-pool"
-  zone       = "us-central1-b"
-  cluster    = "${google_container_cluster.sbx.name}"
-  node_count = 0
+resource "google_container_node_pool" "primary" {
+  name       = "${var.cluster_name}-primary-nodes"
+  zone       = "${local.primary_gcp_zone}"
+  cluster    = "${google_container_cluster.primary.name}"
 
+  node_count = "${max(0, var.primary_pool_node_count - 1)}"
   node_config {
     disk_size_gb = "${var.node_disk_size_gb}"
     disk_type    = "${var.node_disk_type}"
-    machine_type = "${lookup(var.node_machine_counts, "g1-small")}"
+    machine_type = "${var.node_machine_type}"
     preemptible  = "${var.node_preemptible}"
 
     oauth_scopes = [
@@ -82,42 +99,54 @@ resource "google_container_node_pool" "sbx" {
   }
 }
 
-resource "null_resource" "local_credential_write" {
-  provisioner "local-exec" {
-    command = "mkdir -p .gcp/"
-  }
-  provisioner "local-exec" {
-    command = "echo '${google_container_cluster.sbx.master_auth.0.client_certificate}' > .gcp/client.crt"
-  }
-  provisioner "local-exec" {
-    command = "echo '${google_container_cluster.sbx.master_auth.0.client_key}' > .gcp/client.key"
-  }
-  provisioner "local-exec" {
-    command = "echo '${google_container_cluster.sbx.master_auth.0.cluster_ca_certificate}' > .gcp/ca.crt"
+resource "google_container_node_pool" "secondary" {
+  name    = "${var.cluster_name}-secondary-nodes"
+  cluster = "${google_container_cluster.primary.name}"
+
+  count = "${var.secondary_pool_count}"
+  zone  = "${element(local.secondary_gcp_zones, count.index)}"
+
+  node_count = "${var.secondary_pool_node_count}"
+  node_config {
+    disk_size_gb = "${var.node_disk_size_gb}"
+    disk_type    = "${var.node_disk_type}"
+    machine_type = "${var.node_machine_type}"
+    preemptible  = "${var.node_preemptible}"
+
+    oauth_scopes = [
+      "compute-rw",
+      "storage-ro",
+      "logging-write",
+      "monitoring",
+    ]
   }
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
+resource "null_resource" "kubectl_setup" {
+  # Call out to gcloud, which writes kubectl configs
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${google_container_cluster.primary.zone} --project ${var.gcp_project}"
+  }
+}
+
+
 output "client_certificate" {
-  value = "${google_container_cluster.sbx.master_auth.0.client_certificate}"
+  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
 }
 
 output "client_key" {
-  value = "${google_container_cluster.sbx.master_auth.0.client_key}"
+  value = "${google_container_cluster.primary.master_auth.0.client_key}"
 }
 
 output "cluster_ca_certificate" {
-  value = "${google_container_cluster.sbx.master_auth.0.cluster_ca_certificate}"
+  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
 
+
 provider "kubernetes" {
-  host = "${google_container_cluster.sbx.endpoint}"
+  host = "${google_container_cluster.primary.endpoint}"
 
-  # client_certificate     = "${file("~/.kube/client-cert.pem")}"
-  # client_key             = "${file("~/.kube/client-key.pem")}"
-  # cluster_ca_certificate = "${file("~/.kube/cluster-ca-cert.pem")}"
-
-  client_certificate = "${google_container_cluster.sbx.master_auth.0.client_certificate}"
-  client_key = "${google_container_cluster.sbx.master_auth.0.client_key}"
-  cluster_ca_certificate = "${google_container_cluster.sbx.master_auth.0.cluster_ca_certificate}"
+  client_certificate = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+  client_key = "${google_container_cluster.primary.master_auth.0.client_key}"
+  cluster_ca_certificate = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
