37c7665c52ffc314166451c3547b681a9bff3fcc
Adam Shannon <adamkshannon@gmail.com>
2018-11-28 13:06:29 -0600

    docs: add doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2018 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+// Package infra is a collection of libraries and CLI tools used to automate
+// Moov.io's deployment and infrastructure services.
+package infra
