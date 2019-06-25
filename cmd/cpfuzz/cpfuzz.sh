b65b2fb4e76d81f30e79ebce64c66375492c9fc4
Adam Shannon <adamkshannon@gmail.com>
2019-06-25 15:11:38 -0500

    cmd/cpfuzz: fix case where grep returns empty and errors out
    Finding no files was erroring the entire for loop before.

--- cmd/cpfuzz/cpfuzz.sh
+++ cmd/cpfuzz/cpfuzz.sh
@@ -3,6 +3,7 @@ set -e
 
 dir="fuzz-"$(date +"%Y-%m-%d")
 containers=($(kubectl get pods -n apps | grep fuzz | cut -d' ' -f1))
+echo "found ${#containers[@]} fuzz containers"
 
 for container in "${containers[@]}"
 do
@@ -10,10 +11,10 @@ do
     echo "downloading $name fuzz data from $container"
     mkdir -p "$dir"/"$name"
 
-    files=($(kubectl exec -n apps "$container" -- ls -1 /go/src/github.com/moov-io/"$name"/test/fuzz-reader/crashers/ | grep -v 'lost+found'))
+    files=($(kubectl exec -n apps "$container" -- ls -1 /go/src/github.com/moov-io/"$name"/test/fuzz-reader/crashers/))
     for file in "${files[@]}"
     do
-        kubectl cp apps/"$container":/go/src/github.com/moov-io/"$name"/test/fuzz-reader/crashers/"$file" "$dir"/"$name"
+        kubectl cp apps/"$container":/go/src/github.com/moov-io/"$name"/test/fuzz-reader/crashers/"$file" "$dir"/"$name" > /dev/null
     done
 done
 
