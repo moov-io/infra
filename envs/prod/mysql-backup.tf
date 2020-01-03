193a6ca9da5b6fab7fb277529a242578822a946f
Adam Shannon <adamkshannon@gmail.com>
2020-01-03 16:56:50 -0600

    envs/prod: sales: setup, but disable mysql-backup cron
    There's an outstanding issue with the docker container, but pushing up
    since I've created the bucket and CronJob already.

---
+++ envs/prod/mysql-backup.tf
@@ -0,0 +1,79 @@
+resource "google_kms_key_ring" "mysql_backup" {
+  name     = "moov-mysql-backup-key"
+  project  = var.gcp_project
+  location = var.gcp_region
+
+  depends_on = [google_project_services.ach]
+}
+
+resource "google_kms_key_ring_iam_binding" "mysql_backup_key_ring" {
+  key_ring_id = google_kms_key_ring.mysql_backup.self_link
+  role        = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = sort(
+    flatten(
+      [
+        local.project_service_account_emails,
+        local.gcp_cluster_admin_emails,
+      ],
+    ),
+  )
+}
+
+resource "google_kms_crypto_key" "mysql_backup" {
+  name            = "moov-mysql-backup"
+  key_ring        = google_kms_key_ring.mysql_backup.self_link
+  rotation_period = "100000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Attach the key to our IAM users
+# Docs: https://cloud.google.com/kms/docs/iam#granting_permissions_to_use_keys
+resource "google_kms_crypto_key_iam_binding" "mysql_backup_crypto_key" {
+  crypto_key_id = google_kms_crypto_key.mysql_backup.self_link
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = sort(
+    flatten(
+      [
+        local.project_service_account_emails,
+        local.gcp_cluster_admin_emails,
+      ],
+    ),
+  )
+}
+
+resource "google_storage_bucket" "mysql_backups" {
+  name     = "moov-production-mysql-backups"
+  location = var.gcp_region
+
+  force_destroy = false
+  project       = var.gcp_project
+  storage_class = "REGIONAL"
+
+  versioning {
+    enabled = true
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.mysql_backup.self_link
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_storage_bucket_iam_binding" "mysql_backup" {
+  bucket = google_storage_bucket.tf-state-storage.name
+  role   = "roles/storage.objectAdmin"
+  members = sort(
+    flatten(
+      [
+        local.project_service_account_emails,
+        local.gcp_cluster_admin_emails,
+      ],
+    ),
+  )
+}
