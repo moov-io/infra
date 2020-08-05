37cad253a3d12d6e41d87521617819aa46fb47bb
Adam Shannon <adamkshannon@gmail.com>
2020-08-05 16:50:09 -0500

    apps: remove api deployment and routing config

--- envs/oss/moov.io.tf
+++ envs/oss/moov.io.tf
@@ -83,15 +83,6 @@ data "kubernetes_service" "traefik" {
   }
 }
 
-resource "google_dns_record_set" "api" {
-  name         = "api.${google_dns_managed_zone.moov-io.dns_name}"
-  managed_zone = google_dns_managed_zone.moov-io.name
-  type         = "A"
-  ttl          = 60
-
-  rrdatas = [data.kubernetes_service.traefik.load_balancer_ingress[0].ip]
-}
-
 resource "google_dns_record_set" "demo" {
   name         = "demo.${google_dns_managed_zone.moov-io.dns_name}"
   managed_zone = google_dns_managed_zone.moov-io.name
