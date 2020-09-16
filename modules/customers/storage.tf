bcca027c308d42520885fa8e6b3322638f2d1124
Adam Shannon <adamkshannon@gmail.com>
2020-09-16 14:01:47 -0500

    modules/customers: only create persistent volume for SQLite

--- modules/customers/storage.tf
+++ modules/customers/storage.tf
@@ -1,7 +1,11 @@
 resource "kubernetes_persistent_volume_claim" "data" {
+  count = var.database_type == "sqlite" ? 1 : 0
   metadata {
     name = "customers"
     namespace = var.namespace
+  }
+  lifecycle {
+    prevent_destroy = true
   }
   spec {
     access_modes = ["ReadWriteOnce"]
