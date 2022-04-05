17fc2a524867e8a7e94b3bb132f90f0a77249c17
Danny <danny@moov.io>
2022-04-05 12:42:53 -0400

    nancy: only include compiled pkgs in check

--- go/lint-project.sh
+++ go/lint-project.sh
@@ -117,7 +117,7 @@ if [[ "$OS_NAME" != "windows" ]]; then
     ./bin/nancy --clean-cache
 
     # Ignore Consul and Vault Enterprise, they need a gocloud.dev release
-    go list -mod=mod -m all | ./bin/nancy --skip-update-check --loud sleuth --exclude-vulnerability "$ignored"
+    go list -json -deps | ./bin/nancy --skip-update-check --loud sleuth --exclude-vulnerability "$ignored"
 
     echo "" # newline
     echo "finished nancy check"
